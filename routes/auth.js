c45fe5c5cf04065aaabc6b28026e3d2d1883ad44
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-01-15 15:24:48 -0500

    login returns jwt, altered auth route, config, added .env

--- routes/auth.js
+++ routes/auth.js
@@ -5,15 +5,26 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
 
-router.post('/login', localAuth, function (req, res) {
-  console.log(req);
-  return res.json(req.user);
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
 });
+
+
 
  
 module.exports = router;
